5bbba438389178d49e4d52d6bede07e225d34303
GeneralFrosa <sennafarras98@gmail.com>
2023-06-13 18:08:53 +0700

    Change Omni > Twrp In VendorSetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a53x-user
-add_lunch_combo omni_a53x-userdebug
-add_lunch_combo omni_a53x-eng
+add_lunch_combo twrp_a53x-user
+add_lunch_combo twrp_a53x-userdebug
+add_lunch_combo twrp_a53x-eng
