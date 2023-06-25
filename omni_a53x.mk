b6f14516942d7329e81ed8434ed56180fcc57815
Frosa <127599287+GeneralFrosa@users.noreply.github.com>
2023-06-25 19:48:08 +0700

    Twrp Build

--- omni_a53x.mk
+++ omni_a53x.mk
@@ -25,7 +25,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit some common TWRP stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/a53x/device.mk)
